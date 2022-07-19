5fd1fa3b6d834a9ce528c98080e301c3f57f2a6a
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-07-19 17:34:20 -0400

    DONE

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+
+function ListingsContainer({everythingDisplayed}) {
+  console.log(everythingDisplayed);
+  const cards = everythingDisplayed.map(listing => <ListingCard key={listing.id} description={listing.description} image={listing.image} location={listing.location}/>)
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {cards}
       </ul>
     </main>
   );
